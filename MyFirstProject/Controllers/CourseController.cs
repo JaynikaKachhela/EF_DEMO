d9f6daab416a12324c23cfcbacdc7b6f27a66b8f
JaynikaKachhela <jaynika@promactinfo.com>
2016-07-09 16:10:36 +0530

    Commiting the master.

---
+++ MyFirstProject/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using MyFirstProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstProject.Controllers
+{
+    public class CourseController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+        public ActionResult Index()
+        {
+            return View(db.Courses.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Name,TotalCredits")] Course course)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course);
+        }
+       
+    }
+}
---
+++ MyFirstProject/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstProject.Models
+{
+    public class Enrollment
+    {
+        [Required]
+        [Key]
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public decimal? Grade { get; set; }
+        public Student student { get; set; }
+        public Course course { get; set; }
+    }
+}
